482500f7fa87ea55333272471a9cf85bdaa3dfcd
XD <mashavaush10@gmail.com>
2026-02-07 11:26:11 +0300

    feat: add error handling

--- storage/storage.py
+++ storage/storage.py
@@ -6,7 +6,9 @@ class Storage:
         self.filename = filename
 
     def load(self) -> list[Task]:
-        with open(self.filename, 'r', encoding='utf-8') as file:
-            data = json.load(file)
-            # добавить обработку ошибок при попытке открытия файла, если файл не найден или битый возвращать пустой список
-            return [Task.from_dict(data) for i in data]
+        try:
+            with open(self.filename, 'r', encoding='utf-8') as file:
+                data = json.load(file)
+                return [Task.from_dict(data) for i in data]
+        except(FileNotFoundError, json.JSONDecodeError):
+            return []
