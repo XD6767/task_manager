9e997d78a97c38e0582b1fea915bb47fb2f721c1
XD <mashavaush10@gmail.com>
2026-02-14 17:08:10 +0300

    feat: add add_task

--- service/service.py
+++ service/service.py
@@ -4,3 +4,7 @@ class TaskService:
     def __init__(self, storage):
         self.storage = storage
         self.tasks: list[Task] = self.storage.load()
+
+    def add_task(self, object_task):
+        self.tasks.append(object_task)
+        self.storage.save(self.tasks)
