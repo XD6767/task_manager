f73f02987567353b0cd38c6b5299afe732b9e96e
XD <mashavaush10@gmail.com>
2026-02-14 17:41:16 +0300

    feat: add edit_task

--- service/service.py
+++ service/service.py
@@ -13,3 +13,16 @@ class TaskService:
         if index >= 0 and index < len(self.tasks):
             return self.tasks.pop(index)
         return False
+    
+    def edit_task(self, index, new_name, new_priority, new_deadline):
+        if index >= 0 and index < len(self.tasks):
+            task = self.tasks[index]
+            if new_name:
+                task.name = new_name
+            if new_priority:
+                task.priority = new_priority
+            if new_deadline:
+                task.deadline = new_deadline
+            self.storage.save(self.tasks)
+            return True
+        return False
