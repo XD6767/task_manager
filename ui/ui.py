40576307813f483b3a9c608e5f4809383695e52a
XD <mashavaush10@gmail.com>
2026-02-21 11:39:04 +0300

    feat: add remove_task, edit_task, search_tasks

--- ui/ui.py
+++ ui/ui.py
@@ -3,7 +3,7 @@ from models.task import Task
 class Ui:
     def __init__(self, service):
         self.service = service
-        
+    
     def run(self):
         while True:
             print('меню: ')
@@ -42,6 +42,35 @@ class Ui:
         task_list = self.service.get_all_tasks()
         if len(task_list) > 0:
             for index, task in enumerate(task_list, start=1):
-                print(f'{index}. Название: {task["имя"]} | Приоритет: {task["приоритет"]} | Дедлайн: {task["дедлайн"]}')
+                print(f'{index}. Название: {task["name"]} | Приоритет: {task["priority"]} | Дедлайн: {task["deadline"]}')
+        else:
+            print('нет задач')
+
+    def remove_task(self):
+        self.show_task()
+        task_number = int(input('введите номер задачи: '))-1
+        is_note = self.service.remove_task(task_number)
+        if is_note:
+            print(f'была удалена задача: {is_note.name}')
+        else:
+            print('такой задачи нет')
+
+    def edit_task(self):
+        task_num = int(input('введите номер задачи: ')) - 1
+        new_name = input('введите имя задачи: ')
+        new_priority = input('введите приоритет задачи: ')
+        new_deadline = input('введите дедлайн задачи: ')
+        if self.service.edit_task(task_num, new_name, new_priority, new_deadline):
+            print('задача успешно изменена')
+        else:
+            print('такой задачи нет')
+
+    def search_tasks(self):
+        print('поиск задачи')
+        name = input('введите имя задачи: ').lower()
+        notes = self.service.search_tasks(name)
+        if notes:
+            for index, task in enumerate(notes, start=1):
+                print(f'{index}. Название: {task["name"]} | Приоритет: {task["priority"]} | Дедлайн: {task["deadline"]}')
         else:
             print('нет задач')
