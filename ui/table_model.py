e568ce72d1e30e8475377ea1cac17fe03e331fa2
XD <mashavaush10@gmail.com>
2026-03-28 11:57:51 +0300

    feat: add data

--- ui/table_model.py
+++ ui/table_model.py
@@ -16,3 +16,18 @@ class TableModel(QAbstractTableModel):
         if role == Qt.DisplayRole and orientation == Qt.Horizontal:
             return self._titles[index]
         return None
+    
+    def data(self, index, role=Qt.DisplayRole):
+        if not index.isValid():
+            return None
+        if role == Qt.DisplayRole:
+            tasks = self._service.get_all_tasks()
+            task = tasks[index.row()]
+            column = index.column()
+            if column == 0:
+                return task.name
+            elif column == 1:
+                return task.priority
+            elif column == 2:
+                return task.deadline
+        return None
