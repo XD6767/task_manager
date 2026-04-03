7ac6d5a4d57c88db871947a8438263fbb445b5d7
XD <mashavaush10@gmail.com>
2026-04-03 19:14:58 +0300

    refactor: update main

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
 from storage.storage import Storage
 from service.service import TaskService
-from ui.ui import Ui
+from ui.table_model import TableModel
+
+import sys
+from Pyside6.QtWidgets import QMainWindow, QTableView, QApplication
 filename = './task.json'
 
 def runner():
+    app = QApplication([])
     storage = Storage(filename)
     service = TaskService(storage)
-    ui = Ui(service)
-    ui.run()
+
+    window = QMainWindow()
+    table = QTableView()
+    model = TableModel(service)
+    table.setModel(model)
+    
+    window.setCentralWidget(table)
+    window.resize(800, 800)
+    window.show()
+    sys.exit(app.exec())
 
 if __name__ == '__main__':
     runner()
