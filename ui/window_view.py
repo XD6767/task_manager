d0a1be1f8d9c9fab62f6be038a1c9fce3ccee4b9
XD <mashavaush10@gmail.com>
2026-04-11 11:50:59 +0300

    feat: add main_layout

--- ui/window_view.py
+++ ui/window_view.py
@@ -1,4 +1,4 @@
-from PySide6.QtWidgets import QMainWindow, QTableView
+from PySide6.QtWidgets import QMainWindow, QTableView, QPushButton, QVBoxLayout, QWidget
 from storage.storage import Storage
 from service.service import TaskService
 from ui.table_model import TableModel
@@ -15,5 +15,25 @@ class MainWindow(QMainWindow):
         self.model = TableModel(self.service)
         self.table = QTableView()
         self.table.setModel(self.model)
-        self.setCentralWidget(self.table)
+
+        self.add_button = QPushButton('йцукенгшщз')
+        self.add_button.clicked.connect(self.add_task)
         
+        main_layout = QVBoxLayout()
+        main_layout.addWidget(self.table)
+        main_layout.addWidget(self.add_button)
+
+        container = QWidget()
+        container.setLayout(main_layout)
+
+        self.setCentralWidget(container)
+        
+    # создать метод add_task 
+    # метод вызывает добавление задачи из сервиса(не забудь передать аргументы)
+    # вызвать обновление модели таблицы
+
+    # найти информацию про QVBoxLayout(как работает, зачем нужен)
+
+    def add_task(self):
+        self.service.add_task('name', 'priority', 'deadline')
+        self.model.refresh()
